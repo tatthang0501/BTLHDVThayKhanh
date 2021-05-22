3e2a5690836c70cd0e421a9f0bf86cd6a65171bf
tatthang0501 <tatthang0501@gmail.com>
2021-05-23 05:03:08 +0700

    a

--- src/main/java/hdvproject/api/GetSchedule.java
+++ src/main/java/hdvproject/api/GetSchedule.java
@@ -24,7 +24,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import hdvproject.models.LichHoc;
 
 import hdvproject.models.SinhVien;
 
